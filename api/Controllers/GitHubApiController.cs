514962cbc7b7eaf9aa005bf041ee883da1c28134
Jéssika Sousa <84594383+jessikasousa@users.noreply.github.com>
2022-08-14 20:44:08 -0300

    Update GitHubApiController.cs
    fix: access path

--- api/Controllers/GitHubApiController.cs
+++ api/Controllers/GitHubApiController.cs
@@ -1,18 +1,22 @@
 using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
 using System.Text.Json;
-
+using System.Threading.Tasks;
 namespace api.Controllers
 {
     [ApiController]
     [Route("[controller]")]
-    public class GitHubApiController : ControllerBase
+    public class GithubController : ControllerBase
     {
-        [HttpGet]
-         public async Task<IEnumerable<GitHubApiModel>> GetRepositoriesGithub(string profile, int pageNumber, string language, string repositoryName, DateTime createdAt)
-        {
 
-            var request = new HttpRequestMessage(HttpMethod.Get, "https://api.github.com/orgs/takenet/repos");
-            request.Headers.Add("accept", "application/vnd.github+json");
+        [HttpGet]
+        public async Task<IEnumerable<GitHubApiModel>> GetRepositoriesGithub(string profile, int pageNumber, string repositoryName, DateTime createdAt)
+        {
+            var request = new HttpRequestMessage(HttpMethod.Get, "https://api.github.com/users/" + profile + "/repos?" + "page=" + pageNumber);
+            request.Headers.Add("Accept", "application/vnd.github.v3+json");
             request.Headers.Add("User-Agent", "jessikasousa");
 
             var client = new HttpClient();
@@ -23,9 +27,6 @@ namespace api.Controllers
                 var responseStream = await response.Content.ReadAsStreamAsync();
                 var list = await JsonSerializer.DeserializeAsync
                     <IEnumerable<GitHubApiModel>>(responseStream);
-
-                if (!String.IsNullOrEmpty(language))
-                    list = list.Where(x => x.Language == language);
 
                 if (createdAt > DateTime.MinValue)
                     list = list.Where(x => x.CreatedAt.Substring(0, 10) == createdAt.ToString("yyy-MM-dd").Substring(0, 10));
